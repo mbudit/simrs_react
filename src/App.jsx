fa7ccd3ab79a0bb03cad2db47f445393547e87ce
mbudit <buditriono13@gmail.com>
2025-04-24 14:05:34 +0700

    Menambahkan routes ke placeholder pages di App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,17 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Profile from "./pages/Profile";
 import Settings from "./pages/Settings";
-
+import Messages from "./pages/Messages";
+import Notifications from "./pages/Notifications";
+import Analytics from "./pages/Analytics";
+import Calendar from "./pages/Calendar";
+import Library from "./pages/Library";
+import Documents from "./pages/Documents";
+import Search from "./pages/Search";
+import Favorites from "./pages/Favorites";
+import Starred from "./pages/Starred";
+import Help from "./pages/Help";
+ 
 
 function App() {
   return (
@@ -19,6 +29,16 @@ function App() {
               <Route path='/home' element={<Home />} />
               <Route path='/profile' element={<Profile />} />
               <Route path='/settings' element={<Settings />} />
+              <Route path="/messages" element={<Messages />} />
+              <Route path="/notifications" element={<Notifications />} />
+              <Route path="/analytics" element={<Analytics />} />
+              <Route path="/calendar" element={<Calendar />} />
+              <Route path="/library" element={<Library />} />
+              <Route path="/documents" element={<Documents />} />
+              <Route path="/search" element={<Search />} />
+              <Route path="/favorites" element={<Favorites />} />
+              <Route path="/starred" element={<Starred />} />
+              <Route path="/help" element={<Help />} />
               {/* Add more routes matching your menuItems */}
             </Routes>
           </div>

e279df186d4544e6ecde0f711f5fe09775f23bd7
mbudit <buditriono13@gmail.com>
2025-05-02 16:57:40 +0700

    Placeholder table di Profile.jsx dan Revisi NavItem.jsx

--- src/components/NavItem.jsx
+++ src/components/NavItem.jsx
@@ -11,12 +11,13 @@ const NavItem = ({
     return (
         <Link
             to={`${routerLink}`}
+            className='w-full'
 
         >
             <div className='flex items-center gap-5 cursor-pointer w-full hover:text-blue-400'>
                 <span
                     
-                    onClick={() => setIsOpen((prev) => !prev)}
+                    // onClick={() => setIsOpen((prev) => !prev)}  
                     data-tooltip-id={!isOpen ? 'sidebar-tooltip' : undefined}
                     data-tooltip-content={!isOpen ? text : undefined}
                     className='text-2xl'

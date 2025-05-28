6efcd71e0d43b3ff4a2d163003613e95aeac3dfb
mbudit <buditriono13@gmail.com>
2025-05-28 15:59:42 +0700

    Rapihin api backend ranap

--- backend/routes/ranapRoutes.js
+++ backend/routes/ranapRoutes.js
@@ -4,5 +4,7 @@ const ranapController = require('../controllers/ranapController');
 
 router.post("/api/rawatinap", ranapController.createRanap);
 router.get("/api/pasien_ranap", ranapController.getRanapPatients);
+router.put("/api/update_ranap/:id", ranapController.updateRanapPatient);
+router.delete("/api/pasien_ranap/:id", ranapController.deleteRanapPatient);
 
 module.exports = router;

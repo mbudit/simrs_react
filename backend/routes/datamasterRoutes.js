8601081204f444b0e4ae84edfe0e5390aa421bdd
mbudit <buditriono13@gmail.com>
2025-08-05 17:54:59 +0700

    Resolve Conflict di datamasterroutes

--- backend/routes/datamasterRoutes.js
+++ backend/routes/datamasterRoutes.js
@@ -26,13 +26,14 @@ router.get("/api/getLokasi", datamasterController.getLokasi);
 router.delete("/api/deleteLokasi/:id", datamasterController.deleteLokasi);
 router.put("/api/updateLokasi/:id", datamasterController.updateLokasi);
 
-////// Controller for Dokter //////
-router.post("/api/dokter", datamasterController.createDokter);
-router.get("/api/dokter", datamasterController.getDokter);
 ////// Controller for Paramedis //////
 router.post("/api/createParamedis", datamasterController.createParamedis);
 router.get("/api/getParamedis", datamasterController.getParamedis);
 router.delete("/api/deleteParamedis/:id", datamasterController.deleteParamedis);
 router.put("/api/updateParamedis/:id", datamasterController.updateParamedis);
+
+////// Controller for Dokter //////
+router.post("/api/dokter", datamasterController.createDokter);
+router.get("/api/dokter", datamasterController.getDokter);
 
 module.exports = router;
